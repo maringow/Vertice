5d9b9a0e506f71ade28909e3495a43f48d5c44d9
dmagestro <magestro@gmail.com>
2019-06-21 07:57:27 -0500

    commit

--- model.py
+++ model.py
@@ -1,14 +1,36 @@
-# read user input
+import operator
+import numpy as np
+import matplotlib as mpl
+import datetime as dt
+import pandas as pd
+
+
+## read user input
+
+# Placeholders, to be potentially replaced by inputs
+model_type = 'BWAC'
+
+
+## ingest data (IMS, ProspectoRx)
 
 
 
-# ingest data (IMS, ProspectoRx)
+## FINANCIAL CALCULATIONS
+# For each year
+#    Perform year-wise financial calculations
+# Perform present-value calculations (NPV, IRR, etc.)
+
+
+# Placeholder variables, to be replaced later
+discount_rate = 0.15
+tax_rate = 0.21
+
+
+# Placeholder series, to be replaced later
+years = list(range(2015, 2030, 1))
+sales = pd.Series(index=years)
 
 
 
-# financial calculations
-
-
-
-# output
+## output
 
