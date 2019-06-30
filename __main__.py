117148c72c7ff68cab04fed15700383f494368ca
maringow <mgow@westmonroepartners.com>
2019-06-30 13:25:43 -0500

    moved first window to gui and started transitioning to main

--- __main__.py
+++ __main__.py
@@ -11,8 +11,33 @@ from tkinter import ttk
 import gui
 
 
+##----------------------------------------------------------------------
+## INGEST DATA (IMS, ProspectoRx)
 
+# ingest IMS and price data
+IMS = pd.read_csv('sample_8_molecules_w_product.csv')
+prospectoRx = pd.read_csv('gleevec_prospectorx.csv')
+
+# get valid brands from IMS file
+brands = sorted(IMS['Product Sum'].unique())
+
+
+##----------------------------------------------------------------------
+## OPEN WINDOW1
 window = Tk()
-window1 = gui.window1(window)
+window1 = gui.window1(window, brands)
 window.mainloop()
-print(window1.s)
+print('saved variable: {}'.format(window1.parameters['brand_name']))
+
+
+##----------------------------------------------------------------------
+## FIND THERAPEUTIC EQUIVALENTS
+
+# pull records that are therapeutic equivalents of selected brand name drug
+# find Combined Molecule and Prod Form 3 of selected brand name drug; store in lists in case there are multiple
+combined_molecules = IMS.loc[IMS['Product Sum'] == parameters['brand_name']]['Combined Molecule'].unique()
+dosage_forms = IMS.loc[IMS['Product Sum'] == parameters['brand_name']]['Prod Form3'].unique()
+
+# find all IMS records that match the Combined Molecule and Prod Form 3
+df_equivalents = IMS.loc[(IMS['Combined Molecule'].isin(combined_molecules)) & (IMS['Prod Form3'].isin(dosage_forms))]
+print(df_equivalents)
--- gui.py
+++ gui.py
@@ -1,13 +1,15 @@
+import __main__
 from tkinter import *
+from tkinter import ttk
 
 ##----------------------------------------------------------------------
 ## WINDOW 1: SELECT BRAND NAME
 
 
 class window1:
-    brand = ''
+    parameters = {}
 
-    def __init__(self, master):
+    def __init__(self, master, brands):
         self.master = master
         master.title("Generics Forecasting Model")
         master.geometry("600x400")
@@ -16,23 +18,75 @@ class window1:
         self.title = Label(master, text='Generics Forecasting Model: Brand Selection')
         self.title.pack(pady=10)
 
-        self.v = StringVar()
-        self.entry = Entry(master, textvariable=self.v)
-        self.entry.pack()
+        # add label and combobox for brand selection
+        self.brand_label = Label(master, text='Select a brand name drug: ')
+        self.brand_label.pack()
+        self.brand_combo = ttk.Combobox(master, values=brands)
+        self.brand_combo.pack()
+        self.brand_combo.bind("<<ComboboxSelected>>", self.collect_entry_fields)
+
+        # add Finish button
+        self.continue_button = Button(master, text='Continue', command=master.quit)
+        self.continue_button.pack(pady=10)
+
+    # function to collect field entries and store as variables
+    def collect_entry_fields(self, event):
+        self.parameters['brand_name'] = self.brand_combo.get()
+        print(self.parameters['brand_name'])
 
 
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
+    #
+    # # define function to collect field entries and store as variables
+    # def collect_entry_fields(event):
+    #     parameters['brand_name'] = brand_combo.get()
+    #
+    #
+    # # get valid brands from IMS file
+    # brands = sorted(IMS['Product Sum'].unique())
+    #
+    # # add label and combobox for brand selection
+    # brand_label = Label(window1, text='Select a brand name drug: ')
+    # brand_label.pack()
+    # brand_combo = ttk.Combobox(window1, values=brands)
+    # brand_combo.pack()
+    # brand_combo.bind("<<ComboboxSelected>>", collect_entry_fields)
+    #
+    #
+    #
+    #
+    # print('Brand: {}'.format(parameters['brand_name']))
 
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
 
-    def greet(self):
-        print("Greetings!")
-        self.s=self.entry.get()
-        print(self.s)
+##----------------------------------------------------------------------
+## WINDOW 2: CONFIRM BRAND
 
-    def cycle_label_text(self, event):
-        self.label_index += 1
+
+
+
+
+
+
+
+##----------------------------------------------------------------------
+## WINDOW 3: ENTER MODEL PARAMETERS
+
+
+
+
+
+
+
+##----------------------------------------------------------------------
+## WINDOW 4: ENTER API COGS
+
+
+
+
+
+
+
+
+##----------------------------------------------------------------------
+## WINDOW 5: ENTER FINANCIAL ASSUMPTIONS
 
