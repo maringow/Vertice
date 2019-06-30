17c4501a0323f70b504c871c1354352130b1c66b
maringow <mgow@westmonroepartners.com>
2019-06-30 13:34:13 -0500

    set up window2

--- __main__.py
+++ __main__.py
@@ -25,7 +25,7 @@ brands = sorted(IMS['Product Sum'].unique())
 ##----------------------------------------------------------------------
 ## OPEN WINDOW1
 window = Tk()
-window1 = gui.window1(window, brands)
+window1 = gui.Window1(window, brands)
 window.mainloop()
 print('saved variable: {}'.format(window1.parameters['brand_name']))
 
@@ -35,9 +35,16 @@ print('saved variable: {}'.format(window1.parameters['brand_name']))
 
 # pull records that are therapeutic equivalents of selected brand name drug
 # find Combined Molecule and Prod Form 3 of selected brand name drug; store in lists in case there are multiple
-combined_molecules = IMS.loc[IMS['Product Sum'] == parameters['brand_name']]['Combined Molecule'].unique()
-dosage_forms = IMS.loc[IMS['Product Sum'] == parameters['brand_name']]['Prod Form3'].unique()
+combined_molecules = IMS.loc[IMS['Product Sum'] == window1.parameters['brand_name']]['Combined Molecule'].unique()
+dosage_forms = IMS.loc[IMS['Product Sum'] == window1.parameters['brand_name']]['Prod Form3'].unique()
 
 # find all IMS records that match the Combined Molecule and Prod Form 3
 df_equivalents = IMS.loc[(IMS['Combined Molecule'].isin(combined_molecules)) & (IMS['Prod Form3'].isin(dosage_forms))]
 print(df_equivalents)
+
+
+##----------------------------------------------------------------------
+## OPEN WINDOW2
+window = Tk()
+window2 = gui.Window2(window, df_equivalents)
+window.mainloop()
--- gui.py
+++ gui.py
@@ -6,7 +6,7 @@ from tkinter import ttk
 ## WINDOW 1: SELECT BRAND NAME
 
 
-class window1:
+class Window1:
     parameters = {}
 
     def __init__(self, master, brands):
@@ -31,41 +31,27 @@ class window1:
 
     # function to collect field entries and store as variables
     def collect_entry_fields(self, event):
+
         self.parameters['brand_name'] = self.brand_combo.get()
         print(self.parameters['brand_name'])
-
-
-
-    #
-    # # define function to collect field entries and store as variables
-    # def collect_entry_fields(event):
-    #     parameters['brand_name'] = brand_combo.get()
-    #
-    #
-    # # get valid brands from IMS file
-    # brands = sorted(IMS['Product Sum'].unique())
-    #
-    # # add label and combobox for brand selection
-    # brand_label = Label(window1, text='Select a brand name drug: ')
-    # brand_label.pack()
-    # brand_combo = ttk.Combobox(window1, values=brands)
-    # brand_combo.pack()
-    # brand_combo.bind("<<ComboboxSelected>>", collect_entry_fields)
-    #
-    #
-    #
-    #
-    # print('Brand: {}'.format(parameters['brand_name']))
 
 
 ##----------------------------------------------------------------------
 ## WINDOW 2: CONFIRM BRAND
 
 
+class Window2:
 
+    def __init__(self, master, brands):
+        self.master = master
+        master.title("Generics Forecasting Model")
+        master.geometry("600x400")
 
+        # add label
 
-
+        # add Finish button
+        self.continue_button = Button(master, text='Continue', command=master.quit)
+        self.continue_button.pack(pady=10)
 
 
 ##----------------------------------------------------------------------
