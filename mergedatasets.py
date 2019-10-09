9da05bceb485a869d6934b887c8e02eaffc3b195
maringow <mgow@westmonroepartners.com>
2019-10-09 18:25:24 -0500

    broke up mergedatasets into smaller functions and added docstrings

--- mergedatasets.py
+++ mergedatasets.py
@@ -35,37 +35,21 @@ def get_equiv(IMS, parameters):
                    (IMS['Vertice Dosage Form'].isin(parameters['dosage_forms']))]
 
 
+def strip_non_numeric(df_column):
+    df_column = df_column.str.replace('[^0-9]', '')
+    df_column = pd.to_numeric(df_column)
 
-def merge_ims_prospecto(df_equivalents, prospectoRx):
+    return df_column
+
+
+def fill_missing_prices(df_merged_data):
     """
-    Join IMS and prospecto data.
-    Creates molecule- and year-level dataframe.
-
+    For NDCs where no price is found in the ProspectoRx data, attempts to intelligently fill prices by looking
+    for the lowest price among NDCs with the same pack size. If none are found, takes lowest WAC price overall.
+    :param df_merged_data: Dataframe containing IMS and ProspectoRx data for all NDCs identified as equivalents
+    to the brand/molecule selected by user
+    :return: df_merged_data updated with prices
     """
-    def strip_non_numeric(df_column):
-        df_column = df_column.str.replace('[^0-9]', '')
-        df_column = pd.to_numeric(df_column)
-        return df_column
-
-    ##############################################################
-    # parse NDC columns from IMS and ProspectoRx
-    ##############################################################
-    df_equivalents['NDC'] = strip_non_numeric(df_equivalents['NDC'].str.split('\s', expand=True)[0])
-    df_equivalents['NDC'].fillna(999, inplace=True)  # e.g. if NDC is "NDC NOT AVAILABLE"
-    df_equivalents['NDC'] = df_equivalents['NDC'].astype(np.int64)
-    prospectoRx['WACPrice'] = round(prospectoRx['Package Size'] * prospectoRx['WAC (Unit)'], 2)
-    prospectoRx.rename(index=str, columns={'Drug Identifier': 'NDC'}, inplace=True)
-    # prospectoRx['NDC'] = strip_non_numeric(prospectoRx['NDC'])
-
-    ##############################################################
-    # join price and therapeutic equivalents on NDC
-    ##############################################################
-    df_merged_data = df_equivalents.merge(prospectoRx[['NDC', 'WACPrice']], how='left', on='NDC')
-
-    ##############################################################
-    # fill blank prices with lowest WAC price
-    # try by matching pack first, then strength&quantity, otherwise overall min
-    ##############################################################
     for i in df_merged_data.index:
         if math.isnan(df_merged_data['WACPrice'].iloc[i]):
             try:
@@ -74,9 +58,6 @@ def merge_ims_prospecto(df_equivalents, prospectoRx):
                                    df_merged_data['Pack'].iloc[i]]['WACPrice'].dropna())
             except:
                 try:
-                    # x = df_merged_data[df_merged_data['Strength'] == df_merged_data['Strength'].iloc[i]][['WACPrice', 'Pack Quantity']].dropna()  # find same strengths e.g. 100MG
-                    # x["WACPrice_OneUnit_ByStrength"] = x['WACPrice'] / x['Pack Quantity']  # find unit price... price / quantity
-                    # df_merged_data['WACPrice'].iloc[i] = min(x["WACPrice_OneUnit_ByStrength"]) * df_merged_data['Pack Quantity'].iloc[i]  # get price.. unit price * units
                     df_merged_data['WACPrice'].iloc[i] = min(df_merged_data
                                                              [(df_merged_data['Strength'] ==
                                                                df_merged_data['Strength'].iloc[i]) &
@@ -88,30 +69,32 @@ def merge_ims_prospecto(df_equivalents, prospectoRx):
                     except:
                         df_merged_data['WACPrice'].iloc[i] = 0
 
-    ##############################################################
+    return df_merged_data
+
+
+def populate_df_detail(df_equivalents, df_merged_data):
+    """
+    Builds dataframe of years and NDCs (df_detail) and populates with data from df_merged_data
+    :param df_equivalents: Dataframe of IMS data for identified "equivalent" NDCs
+    :param df_merged_data: Dataframe of IMS and ProspectoRx data for equivalent NDCs
+    :return: df_detail, dataframe containing year and NDC-level data used in financial calculations
+    """
     # build hierarchical index on Year and NDC
-    ##############################################################
     year_range = [int(i) for i in np.array(range(2016, 2035))]
     NDCs = [int(i) for i in df_equivalents['NDC'].unique()]
     index_arrays = [year_range, NDCs]
     multiIndex = pd.MultiIndex.from_product(index_arrays, names=['year_index', 'ndc_index'])
 
-    ##############################################################
-    # create df with multiindex
-    ##############################################################
+    # create df_detail with multiindex
     df_detail = pd.DataFrame(index=multiIndex, columns=['NDC', 'Units', 'Price', 'Sales'])
     df_detail['NDC'] = df_detail.index.get_level_values('ndc_index')
 
-    ##############################################################
     # create list of Units columns from IMS data
-    ##############################################################
     columns = [[2016, '2016_Units'], [2017, '2017_Units'], [2018, '2018_Units'],
                [2019, '2019_Units'], [2020, '2020_Units'], [2021, '2021_Units'],
                [2022, '2022_Units']]
 
-    ##############################################################
     # map units and price into df_detail
-    ##############################################################
     for year in columns:
         if year[1] in df_merged_data.columns:
             df_merged_data_agg = df_merged_data[
@@ -125,4 +108,37 @@ def merge_ims_prospecto(df_equivalents, prospectoRx):
     df_detail['Units'] = df_detail['Units'].fillna(0)
     df_detail['Sales'] = df_detail['Units'] * df_detail['Price']
 
-    return (df_merged_data, df_detail)
+    return df_detail
+
+
+def merge_ims_prospecto(df_equivalents, prospectoRx):
+    """
+    Starting with IMS data in df_equivalents, looks up prices from ProspectoRx and stores in df_merged_data and
+    df_detail. If no price is found, function attempts to find the lowest price for the same pack size, otherwise
+    takes lowest price on the market overall.
+    :param df_equivalents: IMS data for NDCs that have been identified as equivalents to the selected brand or molecule
+    :param prospectoRx: Raw price data by NDC
+    :return:
+        df_merged_data: Dataframe containing both price and volume data for all identified equivalent NDCs
+        df_detail: Dataframe indexed on year and NDC, used to hold numbers feeding financial model
+    """
+
+    # parse NDC columns from IMS and ProspectoRx
+    df_equivalents['NDC'] = strip_non_numeric(df_equivalents['NDC'].str.split('\s', expand=True)[0])
+    df_equivalents['NDC'].fillna(999, inplace=True)  # e.g. if NDC is "NDC NOT AVAILABLE"
+    df_equivalents['NDC'] = df_equivalents['NDC'].astype(np.int64)
+    prospectoRx['WACPrice'] = round(prospectoRx['Package Size'] * prospectoRx['WAC (Unit)'], 2)
+    prospectoRx.rename(index=str, columns={'Drug Identifier': 'NDC'}, inplace=True)
+
+    # join price and therapeutic equivalents on NDC
+    df_merged_data = df_equivalents.merge(prospectoRx[['NDC', 'WACPrice']], how='left', on='NDC')
+
+    # fill blank prices with lowest WAC price
+    # try by matching pack first, then strength&quantity, otherwise overall min
+    df_merged_data = fill_missing_prices(df_merged_data)
+    print(df_merged_data)
+
+    # build df_detail and map data in
+    df_detail = populate_df_detail(df_equivalents, df_merged_data)
+
+    return df_merged_data, df_detail
